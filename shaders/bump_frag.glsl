6e159640d295c800ad3179a62872697a779bf055
arfox@rook <arfox@rook>
2009-02-24 19:31:52 -0500

    Almost working (bump mapping) but some coordinate system transdormation is wrong. I don't know which one, though.

--- shaders/bump_frag.glsl
+++ shaders/bump_frag.glsl
@@ -1,47 +1,28 @@
 uniform sampler2D diffuse_map;
 uniform sampler2D normal_map;
 
-varying vec3 nLightDir;
-varying vec3 nHalfVector;
-
-vec4 getSpecular(vec3 n)
-{
-	float nDotHV;
-	vec3 halfVector;
-	
-	nDotHV = max(dot(n, halfVector),0.0);
-	
-	return gl_FrontMaterial.specular *
-	       gl_LightSource[0].specular *
-	       pow(nDotHV, gl_FrontMaterial.shininess);
-}
+varying vec3 LightDir;
+varying vec3 EyeDir;
 
 void main() 
-{
-	vec3 n;
-	float Lambert;
-	vec4 ambient_global, ambient, diffuse, diffuse_tex, specular;
+{	
+	vec4 diffuse_tex = texture2D(diffuse_map, gl_TexCoord[0].st);
 	
-//	diffuse_tex = vec4(1, 1, 1, 1);
-	diffuse_tex = texture2D(diffuse_map, gl_TexCoord[0].st);
-	
-	n = texture2D(normal_map, gl_TexCoord[0].st).xyz;
+	vec3 n = texture2D(normal_map, gl_TexCoord[0].st).xyz;
 	n = (n - 0.5) * 2.0;
 	
-    Lambert = max(dot(n, normalize(nLightDir)), 0.0);
+    float Lambertian = max(dot(n, LightDir), 0.0);
     
-    ambient_global = gl_LightModel.ambient * gl_FrontMaterial.ambient * diffuse_tex;
-    
-	if(Lambert > 0.0) {
-		ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient * diffuse_tex;
-		diffuse = gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse * Lambert * diffuse_tex;
-		specular = getSpecular(n);
-	} else {
-		ambient = vec4(0, 0, 0, 0);
-		diffuse = vec4(0, 0, 0, 0);
-		specular = vec4(0, 0, 0, 0);
-	}
+	vec4 litColor = Lambertian * diffuse_tex; // gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse
+	
+	/*
+	vec3 reflectDir = reflect(LightDir, n);
+	float spec = max(dot(EyeDir, reflectDir), 0.0);
+	spec = pow(spec, 10.0);
+	spec *= 0.5;
+	litColor = min(litColor + spec, vec4(1.0));
+	*/
 
-	gl_FragColor = ambient_global + ambient + diffuse + specular;
+	gl_FragColor = litColor;
 }
 
--- shaders/bump_vert.glsl
+++ shaders/bump_vert.glsl
@@ -1,14 +1,9 @@
-varying vec3 nLightDir;
-varying vec3 nHalfVector;
+varying vec3 LightDir;
+varying vec3 EyeDir;
 
-void main() 
-{	
-	gl_Position = ftransform();
-	gl_TexCoord[0] = gl_MultiTexCoord0;
-	
-	// Calculate the tangent and binormal
-	vec3 tangent; 
-	vec3 binormal; 
+vec3 getTangent()
+{
+	vec3 tangent;
 	vec3 c1 = cross(gl_Normal, vec3(0.0, 0.0, 1.0));
 	vec3 c2 = cross(gl_Normal, vec3(0.0, 1.0, 0.0));
 
@@ -19,15 +14,34 @@ void main()
 	}
 
 	tangent = normalize(tangent);
+	
+	return tangent;
+}
 
-	binormal = cross(tangent, gl_Normal); 
-	binormal = normalize(binormal);
+void main() 
+{	
+	vec3 v;
 	
-	// Transform the light direction to tangent-space
-	nLightDir = mat3(tangent, binormal, gl_Normal) *
-	            normalize(vec4(gl_ModelViewMatrixInverse * gl_LightSource[0].position - gl_Vertex).xyz);
+	gl_Position = ftransform();
+	gl_TexCoord[0] = gl_MultiTexCoord0;
 	
-	// Transform the halfVector to tangent-space
-	nHalfVector = mat3(tangent, binormal, gl_Normal) * vec3(gl_ModelViewMatrixInverse * gl_LightSource[0].halfVector).xyz;
+	// Calculate coordinate-system for surface-local-space 
+	vec3 n = vec3(gl_NormalMatrix * gl_Normal);
+	vec3 t = vec3(gl_NormalMatrix * getTangent());
+	vec3 b = cross(n, t);
+	
+	// Transform the light position from eye to object to surface-local
+	vec3 LightPosition = vec3(gl_ModelViewMatrixInverse * gl_LightSource[0].position);
+	v.x = dot(LightPosition, t);
+	v.y = dot(LightPosition, b);
+	v.z = dot(LightPosition, n);
+	LightDir = normalize(v);
+	
+	// Transform the viewing direction to surface-local
+	EyeDir = vec3(gl_ModelViewMatrix * gl_Vertex);
+	v.x = dot(EyeDir, t);
+	v.y = dot(EyeDir, b);
+	v.z = dot(EyeDir, n);
+	EyeDir = normalize(v);
 }
 
