7a5125db27921618dbfafae1f8bf0c50e730b97e
arfox@WEH5336-T.INTRO.CS.CMU.EDU <arfox@WEH5336-T.INTRO.CS.CMU.EDU>
2009-02-25 22:47:08 -0500

    Forgot these in the last rev :-p

---
+++ shaders/bumpy_chrome_frag.glsl
@@ -0,0 +1,57 @@
+uniform sampler2D normal_map;
+uniform sampler2D env_map;
+
+varying vec3 vertex_to_light;
+varying vec3 eye_to_vertex;
+
+void main()
+{	
+	// Get the color of the environment texture at this fragment
+	vec4 env_map_color = texture2D(env_map, gl_TexCoord[1].st);
+	
+	/* Get the perturbed normal from the normal map.
+	 * This normal can be used in the Lambert term almost directly. We need to
+	 * unpack it from the texture, though.
+	 */
+	vec3 N = texture2D(normal_map, gl_TexCoord[0].st).xyz * 2.0 - 1.0;
+
+	/* The vertex_to_light vector encodes the distance to the light, so we need
+	 * to normalize before using it in the Lambert term.
+	 */
+	vec3 L = normalize(vertex_to_light);
+
+	/* Calculate the Lambert term for this fragment.
+	 * This dot-product drives the rest of the lighting procedure.
+	 */	
+	float NdotL = clamp(dot(N, L), 0.0, 1.0);
+	
+	// The scene has an ambient term separate from any light
+	vec4 ambient_global = gl_LightModel.ambient * gl_FrontMaterial.ambient * env_map_color;
+	
+	// The ambient component from light 0
+	vec4 ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient * env_map_color;
+	
+	/* Calculate light fall-off according to the standard OpenGL light.
+	 * attenuation function.
+	 */
+	float catt = gl_LightSource[0].constantAttenuation;
+	float latt = gl_LightSource[0].linearAttenuation;
+	float qatt = gl_LightSource[0].quadraticAttenuation;
+	float d = length(vertex_to_light);
+	float att = 1.0 / (catt + latt*d + qatt*d*d);
+	
+	// The diffuse component from light 0
+	vec4 diffuse = gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse * env_map_color * NdotL * att;
+
+	// Calculate the specular component from light 0	
+	vec4 specular = vec4(0);
+	if(NdotL > 0.0) {
+		vec3 reflected = reflect(-L, N);
+		float spec = pow(max(0.0, dot(reflected, normalize(eye_to_vertex))), gl_FrontMaterial.shininess);
+		specular = gl_FrontMaterial.specular * gl_LightSource[0].specular * spec;
+	}
+
+	// The final color is the linear combination of the colors computed above.
+	gl_FragColor = ambient_global + ambient + diffuse + specular;
+}
+
---
+++ shaders/bumpy_chrome_vert.glsl
@@ -0,0 +1,43 @@
+attribute vec3 Tangent;
+
+varying vec3 vertex_to_light;
+varying vec3 eye_to_vertex;
+
+void main()
+{
+	vec3 vertex_in_eye_space, eye, r, normal;
+	
+	// First, get some standard computations out of the way
+	gl_Position = ftransform();           // Transform vertex position
+	gl_TexCoord[0] = gl_MultiTexCoord0;   // Send texcoords to the frag shader
+	
+	// Get the reflected eye-vector (eye-space)
+	eye = normalize(vec3(gl_ModelViewMatrix * gl_Vertex));
+	normal = normalize(gl_NormalMatrix * gl_Normal);
+	r = reflect(eye, normal);
+	
+	// Map the uv according to the sphere-mapping eqns on the handout
+	float p = 2.0 * sqrt(r.x*r.x + r.y*r.y + (r.z+1.0)*(r.z+1.0));
+	gl_TexCoord[1].s = r.x/p + 0.5;
+	gl_TexCoord[1].t = r.y/p + 0.5;
+
+	// transform the vertex position into eye-space
+	vertex_in_eye_space = vec3(gl_ModelViewMatrix * gl_Vertex);
+
+	// vector from the vertex to the light (in eye-space)
+	vertex_to_light = vec3(gl_LightSource[0].position) - vertex_in_eye_space;
+	
+	// vector from the vertex to the eye (in eye-space)
+	eye_to_vertex = -vec3(vertex_in_eye_space);
+	
+	// TBN will transform from eye-space to tangent-space
+	vec3 n = normal;
+	vec3 t = vec3(gl_NormalMatrix * -Tangent);
+	vec3 b = cross(n, t);
+	mat3 to_tangent_space = transpose(mat3(t, b, n));
+	
+	// Transform these to tangent-space for the fragment shader
+	vertex_to_light = to_tangent_space * vertex_to_light;
+	eye_to_vertex = to_tangent_space * eye_to_vertex;
+}
+
